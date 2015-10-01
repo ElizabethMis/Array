dc3da17f53d9fdc51ba498f8048ba7a9c35243ac
Elizabeth Mis <elizabethmis@Elizabeths-MacBook-Pro.local>
2015-10-01 16:25:50 -0400

    First Commit

---
+++ ArraySorting.java
@@ -0,0 +1,63 @@
+import java.util.Random;
+/*
+ *	ARRAY SORTING
+ *	
+ *	Write code in the indicated area below to sort the elements of "numbers"
+ *  array in ascending order.
+ *  
+ *	Example:
+ *  The following is example output after a successful implementation:
+ *	
+ *		Before sorting: {70, 67, 86, 49, 99, 70, 4, 15, 88, 78}
+ *		After sorting: {4, 15, 49, 67, 70, 70, 78, 86, 88, 99}	
+*/
+public class ArraySorting {
+	public static void main(String[] args) {
+		int[] numbers = new int[10];  // This is the array to sort
+		Random rand = new Random();
+		System.out.print("Before sorting: {");
+		for(int i = 0; i < numbers.length; i++) {
+			numbers[i] = rand.nextInt(100)+1;
+			System.out.print(numbers[i]);
+			if(i < numbers.length - 1) {
+				System.out.print(", ");
+			}
+		}
+		System.out.println("}");
+		// ∧ ∧ ∧ ∧ ∧ ∧ ∧ LEAVE THE CODE ABOVE THIS LINE ALONE!!! ∧ ∧ ∧ ∧ ∧ ∧ ∧ 
+		
+		// YOUR CODE TO SORT THE numbers ARRAY GOES HERE
+        for(int currentIndex = 0; currentIndex < numbers.length; currentIndex++){
+			int smallestValue = numbers[currentIndex];
+			int smallestIndex = currentIndex;
+		  for(int ix = currentIndex; ix < numbers.length; ix++) {
+	    	  if(numbers[ix] > smallestValue) {
+			  } else if(numbers[ix] < smallestValue) {
+				  smallestValue = numbers[ix];
+				  smallestIndex = ix;
+			  } else { 
+			  }
+			  System.out.println(numbers[currentIndex]+numbers[1]);	
+		  }	
+		  int temp = numbers[currentIndex];
+		  numbers[currentIndex] = numbers[smallestIndex];
+		  numbers[smallestIndex] = temp;
+	    }
+		// ∨ ∨ ∨ ∨ ∨ ∨ ∨ LEAVE THE CODE BELOW THIS LINE ALONE!!! ∨ ∨ ∨ ∨ ∨ ∨ ∨  
+		boolean isWrong = false;
+		System.out.print("After sorting: {");
+		for(int i = 0; i < numbers.length; i++) {
+			System.out.print(numbers[i]);
+			if(i < numbers.length - 1) {
+				System.out.print(", ");
+			}
+			if(i > 0 && numbers[i] < numbers[i-1]) {
+				isWrong = true;
+			}
+		}
+		System.out.println("}");
+		if(isWrong) {
+			System.out.println("Sorry, that's wrong.  Try again.");
+		}
+	}
+}
